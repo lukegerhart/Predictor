a0af63626a0b246a1b48d43b16dc112081400da5
Luke Gerhart <luke_gerhart@hotmail.com>
2018-04-19 00:04:37 -0400

    refactor some stuff, update scores every 15 seconds

--- project/static/js/setup.js
+++ project/static/js/setup.js
@@ -1,29 +1,62 @@
+Array.prototype.peek = function () {
+	return this[this.length-1];
+}
+
+function construct_games(i) {
+	return $("<div>", {class: "game"}).append(
+				$("<table>").append(
+					$("<tr>").append(
+						$("<th>").text("Away"),
+						$("<th>").text("Home")
+					),
+					$("<tr>").append(
+						$("<td>", {class: "team name"}).text(games[i]["awayTeam"]),
+						$("<td>", {class: "team name"}).text(games[i]["homeTeam"])
+					),
+					$("<tr>").append(
+						$("<td>", {class: "team score"}).text(games[i]["awayScore"].peek()),
+						$("<td>", {class: "team score"}).text(games[i]["homeScore"].peek())
+					)
+				)
+			);
+}
+
+function append_game_history(response) {
+	
+	for (var i = 0; i < response["games"]; i++) {
+		
+		for (var k = 0; k < games.length; k++) {
+			if (response["homeTeams"][i] == games[k]["homeTeam"]) {
+				games[k]["clock"].push(response["clock"][i]);
+				games[k]["awayScore"].push(response["awayScores"][i]);
+				games[k]["homeScore"].push(response["homeScores"][i]);
+			}
+		}
+	}
+}
+
 $(document).ready( function() {
-	var numcurrentgames = clocks.length;
-	console.log(numcurrentgames);
+	
 	if (numcurrentgames == 0) {
 		$("#current-games").append($("<p>").text("There are no games going on right now"));
 	} else {
 		var i;
 		for (i = 0; i < numcurrentgames; i++) {
 			$("#current-games").append(
-				$("<div>", {class: "game"}).append(
-					$("<table>").append(
-						$("<tr>").append(
-							$("<th>").text("Away"),
-							$("<th>").text("Home")
-						),
-						$("<tr>").append(
-							$("<td>", {class: "team name"}).text(awayTeams[i]),
-							$("<td>", {class: "team name"}).text(homeTeams[i])
-						),
-						$("<tr>").append(
-							$("<td>", {class: "team score"}).text(awayScores[i]),
-							$("<td>", {class: "team score"}).text(homeScores[i])
-						)
-					)
-				)
+				construct_games(i)
 			);
 		}
 	}
+	
+	setInterval(function() {
+		$.ajax({url:"/games", success: function(response) {
+			append_game_history(response);
+			$("#current-games").empty();
+			for (i = 0; i < games.length; i++) {
+				$("#current-games").append(
+					construct_games(i)
+				);
+			}
+		}});
+	}, 15000);
 });
